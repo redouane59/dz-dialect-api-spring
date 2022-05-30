1477b7cb52e3353a8e96dc712777226226f06f69
Redouane59 <bali.redouane@gmail.com>
2022-05-30 21:28:17 +0100

    init

---
+++ src/main/java/io/github/dzdialectapispring/DzDialectApiSpringApplication.java
@@ -0,0 +1,13 @@
+package io.github.dzdialectapispring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DzDialectApiSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DzDialectApiSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/dzdialectapispring/DzDialectApiSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.dzdialectapispring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DzDialectApiSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
