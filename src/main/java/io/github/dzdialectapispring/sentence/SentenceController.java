edd042b7c456ee0a0c4ac6a519cfd6ad602e520c
Redouane59 <bali.redouane@gmail.com>
2022-05-30 22:07:11 +0100

    suite

--- src/main/java/io/github/dzdialectapispring/sentence/SentenceController.java
+++ src/main/java/io/github/dzdialectapispring/sentence/SentenceController.java
@@ -3,18 +3,24 @@ package io.github.dzdialectapispring.sentence;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/v1/sentences/generate")
+@RequestMapping("api/v1/sentences")
 @AllArgsConstructor
 public class SentenceController {
 
   private final SentenceService sentenceService;
 
-  @GetMapping
+  @GetMapping("/generate")
   public Sentence generateRandomSentence(){
     return sentenceService.generateRandomSentence();
+  }
+
+  @GetMapping("/{id}")
+  public Sentence getSentenceById(@RequestParam String id){
+    return sentenceService.getSentenceById(id);
   }
 
 }
--- src/main/java/io/github/dzdialectapispring/sentence/SentenceService.java
+++ src/main/java/io/github/dzdialectapispring/sentence/SentenceService.java
@@ -2,6 +2,7 @@ package io.github.dzdialectapispring.sentence;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,8 +13,16 @@ public class SentenceService {
 
   private final SentenceRepository sentenceRepository;
 
+  // @todo the random sentence generator should be called there ?
   public Sentence generateRandomSentence(){
     return new Sentence(Set.of(new Translation(Lang.FR,"hello world"),new Translation(Lang.DZ,"Salam Alakyoum")));
   }
 
+  public Sentence getSentenceById(final String id) {
+    Optional<Sentence> sentenceOpt = sentenceRepository.findById(id);
+    if(sentenceOpt.isEmpty()){
+      throw new IllegalArgumentException("No sentence found with id " + id);
+    }
+    return sentenceRepository.findById(id).get();
+  }
 }
